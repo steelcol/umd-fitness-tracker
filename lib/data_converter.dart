2939ce6b51818d8a3fba580aa2b4ef54438b3fb1
eller152 <eller152@umn.edu>
2023-11-01 08:48:41 -0500

    added general event/data classes

---
+++ lib/data_converter.dart
@@ -0,0 +1,23 @@
+// data_converter.dart
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DataConverter {
+  static Map<String, dynamic> eventItemToMap(EventItem event) {
+    return {
+      'name': event.name,
+      'date': Timestamp.fromDate(event.date),
+      'time': event.time,
+      'location': event.location,
+    };
+  }
+
+  static EventItem mapToEventItem(Map<String, dynamic> data) {
+    return EventItem(
+      name: data['name'],
+      date: (data['date'] as Timestamp).toDate(),
+      time: data['time'],
+      location: data['location'],
+    );
+  }
+}
