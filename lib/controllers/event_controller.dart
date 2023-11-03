c98ea41471f98b97082fe4ba11c64d1069985b06
hoyt0084 <hoyt0084@d.umn.edu>
2023-11-02 22:56:07 -0500

    Can create events with this controller.

---
+++ lib/controllers/event_controller.dart
@@ -0,0 +1,19 @@
+import 'package:BetaFitness/models/event_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class EventController {
+  final dbRef = FirebaseFirestore.instance;
+  final user = FirebaseAuth.instance.currentUser;
+
+  // Public Functions
+
+  // Adds an event to the database
+  Future<void> addEvent(Event event) async {
+    await dbRef.collection("Events")
+        .doc(user!.uid)
+        .update({"Events_Array": FieldValue.arrayUnion([{
+          "EventName": event.eventName
+    }])});
+  }
+}
