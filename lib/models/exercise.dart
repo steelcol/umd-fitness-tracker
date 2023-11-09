95429087055997f80323f58e48c6c06eef067369
eller152 <eller152@umn.edu>
2023-11-09 14:41:15 -0600

    added exercise.dart

---
+++ lib/models/exercise.dart
@@ -0,0 +1,14 @@
+// exercise class needs to needs description, exercise name, video url
+
+class Exercise {
+  String name;
+  String description;
+  String videoURL;
+
+  Exercise ({
+    required this.name,
+    required this.description,
+    required this.videoURL,
+  });
+}
+
