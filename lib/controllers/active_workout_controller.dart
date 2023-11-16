e66123f5bd2046d5db6c43ac491f31edb5293fbc
hoyt0084 <hoyt0084@d.umn.edu>
2023-11-16 15:56:21 -0600

    Added some files for active workout prep.

--- lib/controllers/active_workout_controller.dart
+++ lib/controllers/active_workout_controller.dart
@@ -3,4 +3,6 @@ import 'package:BetaFitness/models/weight_workout_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ActiveWorkoutController{}
+class ActiveWorkoutController {
+
+}
