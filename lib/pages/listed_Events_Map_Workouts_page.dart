aa51afdeb97185583d8df9226cbc773764cb390b
eller152 <eller152@umn.edu>
2023-11-19 18:03:49 -0600

    updated UI for listed_Events_Map_Workouts_page.dart

--- lib/pages/listed_Events_Map_Workouts_page.dart
+++ lib/pages/listed_Events_Map_Workouts_page.dart
@@ -1,87 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:BetaFitness/storage/event_storage.dart';
 
-
 class ListedEventsMapWorkoutsPage extends StatefulWidget {
   const ListedEventsMapWorkoutsPage({Key? key, required this.eventStorage}) : super(key: key);
 
   final EventStorage eventStorage;
 
-  State<ListedEventsMapWorkoutsPage> createState() => _ListedEventsMapWorkoutsPageState();
+  @override
+  _ListedEventsMapWorkoutsPageState createState() => _ListedEventsMapWorkoutsPageState();
 }
+
 class _ListedEventsMapWorkoutsPageState extends State<ListedEventsMapWorkoutsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Events: '),
+        title: Text('Your Events'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                'Scroll down to see more text boxes!',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Scroll down to see more text boxes!',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(height: 20.0), //make function to find out how many events there are in selected day?
-              Text('Event: ' + widget.eventStorage.storedEventName),
-              Text('Description: ' + widget.eventStorage.storedEventDescription),
-              Text('Date: ${widget.eventStorage.storedDate.toString()}'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              Text('text'),
-              buildTextBox('Text Box'),
-              // Add more text boxes as needed
-            ],
-          ),
+            ),
+            SizedBox(height: 20.0),
+            buildEventInfo('Event:', widget.eventStorage.storedEventName),
+            buildEventInfo('Description:', widget.eventStorage.storedEventDescription),
+            buildEventInfo('Date:', widget.eventStorage.storedDate.toString()),
+            buildTextBox('Text Box'),
+            // Add more widgets as needed
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget buildEventInfo(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 4.0),
+          Text(
+            value,
+            style: TextStyle(
+              fontSize: 16.0,
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -98,21 +78,3 @@ class _ListedEventsMapWorkoutsPageState extends State<ListedEventsMapWorkoutsPag
     );
   }
 }
-
-
-
-
-/*
-class _ListedEventsMapWorkoutsPageState extends State<ListedEventsMapWorkoutsPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.eventStorage.storedEventName),
-      )
-    );
-    //
-    throw UnimplementedError();
-  }
-}
- */
