31793c125c83938c86bbdd5d71e8af05da86a265
nymoe007 <nymoe007@d.umn.edu>
2023-11-20 19:33:41 -0600

    directions template page changes

--- lib/pages/directions_template_page.dart
+++ lib/pages/directions_template_page.dart
@@ -1,18 +1,56 @@
-String url = 'https://www.google.com/maps/dir/?api=1&origin=' +
-    currentLocation.latitude.toString() +
-    ',' +
-    currentLocation.longitude.toString() +
-    ' &destination=' +
-    lat.toString() +
-    ',' +
-    lon.toString() +
-    '&travelmode=driving&dir_action=navigate';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
-void _launchURL(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
+class DirectionsTemplatePage extends StatefulWidget {
+  DirectionsTemplatePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
+
+  //widget.storage.events[$INDEX_VALUE].location
+
+  String url = 'https://www.google.com/maps/dir/?api=1&origin=' +
+      currentLocation.latitude.toString() +
+      ',' +
+      currentLocation.longitude.toString() +
+      ' &destination=' +
+      lat.toString() + //latitude
+      ',' +
+      lon.toString() + //longitude
+      '&travelmode=driving&dir_action=navigate';
+
+
+  void _launchURL(String url) async {
+    if (await canLaunchUrl(url as Uri)) {
+      await launchUrl(url as Uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('BetaFitness'),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            ElevatedButton(onPressed: () => _launchURL, child: Text("enter"), ),
+            ]
+          ),
+        ),
+    );
+  }
+
 }
