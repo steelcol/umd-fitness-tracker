172d089e7ef36ceed5dd8b285d163f74e075b717
Alexander Towey <towey039@d.umn.edu>
2023-11-20 22:14:36 -0600

    errors but will rollback trying something for bugfixing

--- lib/pages/listed_events_map_workouts_page.dart
+++ lib/pages/listed_events_map_workouts_page.dart
@@ -1,10 +1,14 @@
 import 'package:BetaFitness/models/save_data_model.dart';
+import 'package:BetaFitness/storage/event_list_storage.dart';
 import 'package:flutter/material.dart';
+
+import '../storage/event_storage.dart';
 
 class ListedEventsMapWorkoutsPage extends StatefulWidget {
   const ListedEventsMapWorkoutsPage({Key? key, required this.storeDateTime}) : super(key: key);
 
   final StoreDateTime storeDateTime;
+
 
   @override
   _ListedEventsMapWorkoutsPageState createState() => _ListedEventsMapWorkoutsPageState();
@@ -30,10 +34,32 @@ class _ListedEventsMapWorkoutsPageState extends State<ListedEventsMapWorkoutsPag
               ),
             ),
             SizedBox(height: 20.0),
-            buildEventInfo('Event:', widget.storeDateTime.eventStorage.storedEventName),
-            buildEventInfo('Description:', widget.storeDateTime.eventStorage.storedEventDescription),
-            buildEventInfo('Date:', widget.storeDateTime.eventStorage.storedDate.toString()),
+            buildEventInfo(
+                'Event:', widget.storeDateTime.eventStorage.storedEventName),
+            buildEventInfo('Description:',
+                widget.storeDateTime.eventStorage.storedEventDescription),
+            buildEventInfo('Date:',
+                widget.storeDateTime.eventStorage.storedDate.toString()),
             // Add more widgets as needed
+            Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 10,
+              ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: widget.storeDateTime.eventStorage.listOfEvents
+                    .length,
+                itemBuilder: (context, index) {
+                  print(widget.storeDateTime.eventStorage.listOfEvents.length);
+                  return _buildEventCard(
+                      widget.storeDateTime.eventStorage
+                          .listOfEvents[index],
+                      index
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
@@ -58,6 +84,77 @@ class _ListedEventsMapWorkoutsPageState extends State<ListedEventsMapWorkoutsPag
             value,
             style: TextStyle(
               fontSize: 16.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEventCard(EventStorage eventStorage, int index) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            top: 0,
+            bottom: 0,
+            child: Container(
+              width: 2, // Width/color highlight
+              color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(12),
+            child: Row(
+              children: [
+                Text(
+                  "${index + 1}.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 8), // Space from highlight to index
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Event Name: " + widget.storeDateTime.eventStorage.listOfEvents[index].storedEventName,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Description: " + widget.storeDateTime.eventStorage.listOfEvents[index].storedEventDescription,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Date: ${widget.storeDateTime.eventStorage.listOfEvents[index].storedDate}",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
--- lib/storage/event_storage.dart
+++ lib/storage/event_storage.dart
@@ -1,6 +1,9 @@
+import 'package:BetaFitness/storage/event_list_storage.dart';
+
 class EventStorage {
   late String storedEventName; //after iteration function is called it stores the users selected event name in this String
   late String storedEventDescription; //after iteration function is called it stores the users selected event description in this String
-  late bool storeCheck; // used in checking wether you can move to the next page or not
+  late bool storeCheck; // used in checking whether you can move to the next page or not
   late DateTime storedDate;//after iteration function is called it stores the users selected event DateTime in this dateTime
+  late List<EventStorage> listOfEvents = [];
 }
