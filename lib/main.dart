20c92c01206b4efc1ba762895c2455e0a775384d
hoyt0084 <hoyt0084@d.umn.edu>
2023-11-20 19:29:22 -0600

    Added request for location permissions.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:core';
 
+import 'package:geolocator/geolocator.dart';
+
 import 'package:BetaFitness/utilities/route_generator.dart';
 import 'package:BetaFitness/utilities/routes.dart';
 
@@ -14,6 +16,20 @@ Future<void> main() async {
     DeviceOrientation.portraitDown,
     DeviceOrientation.portraitUp
   ]);
+
+  var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale 
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
   runApp(BetaFitness());
 }
 
