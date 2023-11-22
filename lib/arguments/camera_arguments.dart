e7b8ece321222a22451145fa30acf3b6b9b530cc
roell030 <roell030@umn.edu>
2023-11-21 18:44:42 -0600

    Added arguments for the achievements pages

---
+++ lib/arguments/camera_arguments.dart
@@ -0,0 +1,7 @@
+import 'package:camera/camera.dart';
+
+class CameraArguments {
+  final CameraDescription camera;
+
+  CameraArguments({required this.camera});
+}
---
+++ lib/arguments/captured_achievement_arguments.dart
@@ -0,0 +1,9 @@
+import 'package:camera/camera.dart';
+
+class CapturedAchievementArguments {
+  final XFile image;
+
+  CapturedAchievementArguments({
+    required this.image,
+  });
+}
