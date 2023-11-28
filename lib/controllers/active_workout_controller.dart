33c028003c717b1310d4dcf503bafc0d59f03b47
hoyt0084 <hoyt0084@d.umn.edu>
2023-11-28 16:47:22 -0600

    Fixed controllers.

--- lib/controllers/active_workout_controller.dart
+++ lib/controllers/active_workout_controller.dart
@@ -8,16 +8,32 @@ class ActiveWorkoutController {
 
   // Public functions
   void addCompletedWorkout(Map<String, List<int>> weightValues) async {
-    DateTime now = DateTime.now();
-    var uuid = Uuid();
-    await dbRef
-      .doc(userId)
-      .collection('CompletedWorkouts')
-      .doc(userId)
-      .update({'Workouts': FieldValue.arrayUnion([{
-        'Date': DateTime(now.year, now.month, now.day).millisecondsSinceEpoch,
-        'UniqueId': uuid.v4(),
-        'Exercises': weightValues 
-      }])});
+    final snapshot = await dbRef.doc(userId).collection('CompletedWorkouts').get();
+    if (snapshot.size == 0) {
+      DateTime now = DateTime.now();
+      var uuid = Uuid();
+      await dbRef
+        .doc(userId)
+        .collection('CompletedWorkouts')
+        .doc(userId)
+        .set({'Workouts': FieldValue.arrayUnion([{
+          'Date': DateTime(now.year, now.month, now.day).millisecondsSinceEpoch,
+          'UniqueId': uuid.v4(),
+          'Exercises': weightValues 
+        }])});
+    }
+    else {
+      DateTime now = DateTime.now();
+      var uuid = Uuid();
+      await dbRef
+        .doc(userId)
+        .collection('CompletedWorkouts')
+        .doc(userId)
+        .update({'Workouts': FieldValue.arrayUnion([{
+          'Date': DateTime(now.year, now.month, now.day).millisecondsSinceEpoch,
+          'UniqueId': uuid.v4(),
+          'Exercises': weightValues 
+        }])});
+    }
   }
 }
