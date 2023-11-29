dea36858b6441965db4379030f1eadaed2d45716
roell030 <roell030@umn.edu>
2023-11-29 16:41:02 -0600

    Concision of display_captured_achievement_page.dart build function

--- lib/pages/display_captured_achievement_page.dart
+++ lib/pages/display_captured_achievement_page.dart
@@ -44,114 +44,105 @@ class _DisplayCapturedAchievementPageState extends
       ),
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints viewportConstraints) {
-          return SingleChildScrollView(
-            physics: ScrollPhysics(),
-            padding: EdgeInsets.all(5),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxHeight: viewportConstraints.maxHeight,
-              ),
-              child: ListView(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(15),
-                      child: Image.file(
-                        File(widget.image.path),
-                        width: viewportConstraints.maxWidth,
-                        scale: 0.55,
+          return ListView(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Image.file(
+                    File(widget.image.path),
+                    width: viewportConstraints.maxWidth,
+                    scale: 0.55,
+                  ),
+                ),
+                Spacer(),
+                Column(
+                    children: [
+                      TextFormField(
+                        controller: _descriptionField,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Description required';
+                          }
+                          return null;
+                        },
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal,
+                        ),
+                        decoration: InputDecoration(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery
+                                .of(context)
+                                .size
+                                .width * 2 / 3,
+                          ),
+                          labelText: null,
+                          labelStyle: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.normal,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(2.0))
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(2.0)),
+                          ),
+                          floatingLabelBehavior: FloatingLabelBehavior
+                              .always,
+                          hintText: 'Enter Description',
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
                       ),
-                    ),
-                    Spacer(),
-                    Column(
-                        children: [
-                          TextFormField(
-                            controller: _descriptionField,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Description required';
-                              }
-                              return null;
-                            },
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.normal,
-                            ),
-                            decoration: InputDecoration(
-                              constraints: BoxConstraints(
-                                maxWidth: MediaQuery
-                                    .of(context)
-                                    .size
-                                    .width * 2 / 3,
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment
+                                .spaceAround,
+                            children: [
+                              Spacer(),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Theme.of(context).primaryColor,
+                                ),
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text(
+                                  'Retake',
+                                ),
                               ),
-                              labelText: null,
-                              labelStyle: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.normal,
+                              Spacer(),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Theme.of(context).primaryColor,
+                                ),
+                                onPressed: () async {
+                                  _achievementController.addAchievement(
+                                      new Achievement(
+                                        dateCaptured: DateTime.now(),
+                                        description: _descriptionField
+                                            .text,
+                                        image: base64Encode(
+                                            await widget.image
+                                                .readAsBytes()),
+                                      )
+                                  );
+                                  widget.updateList();
+                                  Navigator.of(context).pop();
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Submit'),
                               ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(2.0))
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(2.0)),
-                              ),
-                              floatingLabelBehavior: FloatingLabelBehavior
-                                  .always,
-                              hintText: 'Enter Description',
-                              hintStyle: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(10),
-                            child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment
-                                    .spaceAround,
-                                children: [
-                                  Spacer(),
-                                  ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: Theme.of(context).primaryColor,
-                                    ),
-                                    onPressed: () => Navigator.of(context).pop(),
-                                    child: Text(
-                                      'Retake',
-                                    ),
-                                  ),
-                                  Spacer(),
-                                  ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: Theme.of(context).primaryColor,
-                                    ),
-                                    onPressed: () async {
-                                      _achievementController.addAchievement(
-                                          new Achievement(
-                                            dateCaptured: DateTime.now(),
-                                            description: _descriptionField
-                                                .text,
-                                            image: base64Encode(
-                                                await widget.image
-                                                    .readAsBytes()),
-                                          )
-                                      );
-                                      widget.updateList();
-                                      Navigator.of(context).pop();
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text('Submit'),
-                                  ),
-                                  Spacer(),
-                                ]
-                            ),
-                          ),
-                        ]
-                    ),
-                  ]
-              ),
-            ),
+                              Spacer(),
+                            ]
+                        ),
+                      ),
+                    ]
+                ),
+              ]
           );
         }
       ),
