009b2a9966e0c3416b831e9b6e95a817c185fdfd
eller152 <eller152@umn.edu>
2023-11-29 13:22:34 -0600

    updated UI for entering image descriptions

--- lib/pages/display_captured_achievement_page.dart
+++ lib/pages/display_captured_achievement_page.dart
@@ -1,17 +1,17 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:BetaFitness/controllers/achievement_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:BetaFitness/models/achievement_model.dart';
-import 'dart:io';
 
 class DisplayCapturedAchievementPage extends StatefulWidget {
-  DisplayCapturedAchievementPage({Key? key,
+  DisplayCapturedAchievementPage({
+    Key? key,
     required this.image,
-    required this.updateList
-  })
-      : super(key: key);
+    required this.updateList,
+  }) : super(key: key);
 
   final XFile image;
   final Function updateList;
@@ -21,102 +21,106 @@ class DisplayCapturedAchievementPage extends StatefulWidget {
       _DisplayCapturedAchievementPageState();
 }
 
-class _DisplayCapturedAchievementPageState extends
-      State<DisplayCapturedAchievementPage> {
-
-  final String description = "";
-  final AchievementController _achievementController = new AchievementController();
+class _DisplayCapturedAchievementPageState
+    extends State<DisplayCapturedAchievementPage> {
+  final AchievementController _achievementController =
+  AchievementController();
 
   final TextEditingController _descriptionField =
-      TextEditingController(text: 'New Description');
-
-  @override
-  initState() {
-    super.initState();
-    setState(() {});
-  }
+  TextEditingController(text: 'New Description');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text('BetaFitness')
+        title: const Text('BetaFitness'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Image.file(File(widget.image.path)),
-            Spacer(),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  child: Text('Retake'),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextFormField(
-                        controller: _descriptionField,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Description required';
-                          }
-                          return null;
-                        },
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 12,
-                          fontWeight: FontWeight.normal,
-                        ),
-                        decoration: InputDecoration(
-                          constraints: BoxConstraints(
-                            maxWidth: MediaQuery.of(context).size.width / 2,
-                          ),
-                          labelText: null,
-                          labelStyle: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.normal,
-                          ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                          ),
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-                          hintText: 'Enter Description',
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                        child: Text('Submit'),
-                        onPressed: () async {
-                          _achievementController.addAchievement(
-                            new Achievement(
-                              dateCaptured: DateTime.now(),
-                              description: _descriptionField.text,
-                              image: base64Encode(await widget.image.readAsBytes()),
-                            )
-                          );
-                          widget.updateList();
-                          Navigator.of(context).pop();
-                          Navigator.of(context).pop();
-                        }
-                      ),
-                    ]
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Improved Image Display
+              Image.file(
+                File(widget.image.path),
+                height: 400, // Adjust the image height
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 16),
+              // Reorganized Buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text('Retake'),
                   ),
-                ),
-              ]
-            ),
-          ]
-      ),
+                  SizedBox(width: 16),
+                  // Styled Text Form Field
+                  Expanded(
+                    child: TextFormField(
+                      controller: _descriptionField,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Description required';
+                        }
+                        return null;
+                      },
+                      style: TextStyle(
+                        color: Colors.white, // Changed to white lettering
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'New Description',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white, // Changed to white border
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        hintText: 'Enter Description',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  // Styled Submit Button
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      textStyle: TextStyle(fontSize: 16),
+                    ),
+                    child: Text('Submit'),
+                    onPressed: () async {
+                      _achievementController.addAchievement(
+                        Achievement(
+                          dateCaptured: DateTime.now(),
+                          description: _descriptionField.text,
+                          image: base64Encode(await widget.image.readAsBytes()),
+                        ),
+                      );
+                      widget.updateList();
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
